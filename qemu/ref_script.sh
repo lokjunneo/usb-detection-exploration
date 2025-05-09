b77a29a6974ecfc704d9bc430f31afa1fbc38415
lokjunneo <neolokjun@gmail.com>
2025-05-09 16:48:52 +0800

    Updated readme and ref script

--- qemu/ref_script.sh
+++ qemu/ref_script.sh
@@ -1,29 +1,36 @@
 # Pull image
-# curl -O https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/debian-12.5.0-amd64-netinst.iso
-# curl -O https://dl-cdn.alpinelinux.org/alpine/v3.19/releases/x86_64/alpine-standard-3.19.1-x86_64.iso
-curl -O https://nl.alpinelinux.org/alpine/v3.8/releases/x86_64/alpine-standard-3.8.0-x86_64.iso
 
+# curl -O https://nl.alpinelinux.org/alpine/v3.8/releases/x86_64/alpine-standard-3.8.0-x86_64.iso
+
+# wget https://cdimage.debian.org/cdimage/daily-builds/daily/arch-latest/amd64/iso-cd/debian-testing-amd64-netinst.iso
+# wget https://cdimage.debian.org/debian-cd/current/arm64/iso-cd/debian-12.10.0-arm64-netinst.iso
+wget https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/debian-12.10.0-amd64-netinst.iso
 # Create image
-qemu-img create -f qcow2 alpine.qcow2 16G
+# qemu-img create -f qcow2 alpine.qcow2 16G
+qemu-img create -f qcow2 debian.qcow2 16G
 
 # Install & Boot
+# (Software selection - Only need standard system utilities)
 
 qemu-system-x86_64 \
-    -m 2048 \
-    -nic user,model=virtio \
-    -boot d \
-    -hda alpine.qcow2 \
-    -cdrom alpine-standard-3.8.0-x86_64.iso
-
-# setup-alpine, use "sda" disk with "sys" installation
-# https://wiki.alpinelinux.org/wiki/QEMU#Install_Alpine_Linux_in_QEMU
+  -m 2048 \
+  -nic user,model=virtio \
+  -hda debian.qcow2 \
+  -k en-us \
+  -cdrom debian-12.10.0-amd64-netinst.iso \
+  -boot d \
+  -usb -device usb-ehci
 
 # Boot existing
 qemu-system-x86_64 \
     -m 2048 \
     -nic user,model=virtio \
     -usb -device usb-ehci \
+    -k en-us \
     -boot d \
-    -hda alpine.qcow2
+    -hda debian.qcow2
 
 # ctrl + option + 1 to switch between Qemu Monitor and Linux
+
+# setup-alpine, use "sda" disk with "sys" installation
+# https://wiki.alpinelinux.org/wiki/QEMU#Install_Alpine_Linux_in_QEMU
--- readme.md
+++ readme.md
@@ -8,3 +8,13 @@ Windows only
 
 ## Monitoring /dev or /media filesystem (Linux/macOS)
 
+
+
+## Qemu commands
+
+Simulate adding/disconnecting usb device
+
+```sh
+(qemu) device_add usb-audio,id=mic1,bus=usb-bus.0
+(qemu) device_del mic1
+```
