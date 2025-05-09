a63d5e4076e3956a3dd68354b46162cc24d96368
lokjunneo <neolokjun@gmail.com>
2025-05-09 21:01:01 +0800

    Updated synchronous example to print every property

--- pyudev/synchronous.py
+++ pyudev/synchronous.py
@@ -7,35 +7,11 @@ context = pyudev.Context()
 monitor = pyudev.Monitor.from_netlink(context)
 monitor.filter_by('usb')
 
-possibly_useful_properties = ["DEV_TYPE",
-                              "DRIVER",
-                            "ID_BUS",
-                            "ID_FOR_SEAT",
-                            "ID_MODEL",
-                            "ID_MODEL_ENC",
-                            "ID_REVISION",
-                            "ID_SERIAL",
-                            "ID_SERIAL_SHORT",
-                            "ID_USB_MODEL",
-                            "ID_USB_MODEL_ENC",
-                            "ID_USB_MODEL_ID",
-                            "ID_USB_REVISION",
-                            "ID_USB_SERIAL",
-                            "ID_USB_SERIAL_SHORT",
-                            "ID_USB_VENDOR",
-                            "ID_USB_VENDOR_ENC",
-                            "ID_USB_VENDOR_ID",
-                            "ID_VENDOR",
-                            "ID_VENDOR_ENC",
-                            "ID_VENDOR_FROM_DATABASE",
-                            "ID_VENDOR_ID",
-                              ]
 for device in iter(monitor.poll, None):
     if (device.action == "add"):
         print("+"* 50)
         print("Action:", device.action)
         for i in device.properties.keys():
-        # for i in possibly_useful_properties: 
             if i in device.properties:
                 print(f"{i}: {device.properties[i]}")
         print("+"* 50)
